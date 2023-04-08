5c56be0f8d02c81337f85843db76fa60708e0905
iurii-chernigin <chernigin.y@gmail.com>
2023-04-09 00:48:38 +0300

    schema: add default constructor

--- src/main/java/audio/streaming/schema/StatusChangeEvent.java
+++ src/main/java/audio/streaming/schema/StatusChangeEvent.java
@@ -3,7 +3,9 @@ package audio.streaming.schema;
 import java.util.Map;
 public class StatusChangeEvent extends Event {
 
-    public StatusChangeEvent (String artist, String song, float duration, String page, String method, int status, long ts, long sessionId, String auth, String level, int itemInSession,String city, int zip, String state, String userAgent, float lon, float lat, int userId, String lastName, String firstName, String gender, long registration, String tag) {
+    public StatusChangeEvent() {}
+
+    public StatusChangeEvent (long ts, long sessionId, String auth, String level, int itemInSession, String city, int zip, String state, String userAgent, float lon, float lat, int userId, String lastName, String firstName, String gender, long registration, String tag) {
         super(ts, sessionId, auth, level, itemInSession, city, zip, state, userAgent, lon, lat, userId, lastName, firstName, gender, registration, tag);
     }
 
